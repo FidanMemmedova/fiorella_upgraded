eb9fac36b5864167cb7b5e78d6e5e192b6b22ff9
FidanMemmedova <100687487+FidanMemmedova@users.noreply.github.com>
2022-05-22 23:28:31 +0400

    Add files via upload

---
+++ fiorella/assets/js/main.js
@@ -0,0 +1,5 @@
+// const hamburger_btn = document.querySelector('.hamburger');
+// const navbar = document.querySelector('.sidebar')
+// hamburger_btn.addEventListener('click', () => {
+//     navbar.classList.toggle("change");
+// });
